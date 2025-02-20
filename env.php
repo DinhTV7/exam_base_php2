312f9a89998ccab3a5822b7de622a8c8f662085c
DinhTV7 <dinhtv7@fpt.edu.vn>
2025-02-20 13:22:18 +0700

    First commit

---
+++ env.php
@@ -0,0 +1,11 @@
+<?php
+define('BASE_URL', '');
+// Sửa link theo thư mục trong máy của bạn
+
+define('DB_HOST', 'localhost');
+define('DB_PORT', 3306);
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', ''); // Tên database //
+
+define('PATH_ROOT', __DIR__ . '/');
